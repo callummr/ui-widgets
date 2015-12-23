8c1fa109f9885510fe1ff5980d96b22019a02de3
Luke Zammit <lzammit@macmillan.org.uk>
2015-12-23 14:14:34 +0000

    Cleaned up the base file so it didnt error.

--- projects/template-builder/assets/js/user-landing.js
+++ projects/template-builder/assets/js/user-landing.js
@@ -7,14 +7,14 @@ _$(document).ready(function () {
     // ul = USER LANDING
     app.ul = {
         init: function () {
-            app.ul.xxx();
+            
         },
         xxx: function(){
-            ....
+            
         }
     };
 
-    if(xxx){
+    if(true){
         app.ul.init();   
     }    
 });
