519b7ca1c598b3aa7b526de1d44f17ac2ba79737
Dino Hassiotis <dhassiotis@macmillan.org.uk>
2016-01-04 09:13:48 +0000

    landing page setup

--- projects/template-builder/assets/js/user-landing.js
+++ projects/template-builder/assets/js/user-landing.js
@@ -7,14 +7,87 @@ _$(document).ready(function () {
     // ul = USER LANDING
     app.ul = {
         init: function () {
-            console.log('initiated')
+            console.log('initiated');
+            app.ul.showHide();
+            app.ul.jsonTest();
         },
         xxx: function(){
-            
+
+        },
+        showHide: function () {
+
+            _$('#accordion').find('.panel-title').click(function(e){
+
+                e.preventDefault();
+
+                var title = _$(this).children();
+
+                //Expand or collapse this panel
+                //_$(this).parent().next().slideToggle('fast');
+
+                if( _$(this).parent().next().hasClass('in') ) {
+                    _$(this).parent().next().removeClass('in');
+                } else {
+                    _$(this).parent().next().addClass('in');
+                }
+
+                if(title.attr('aria-expanded') === 'true' ) {
+                    title.attr('aria-expanded', 'false');
+                } else {
+                    title.attr('aria-expanded', 'true');
+                }
+
+                //Hide the other panels
+                //_$(".accordion-content").not(_$(this).next()).slideUp('fast');
+            });
+        },
+        jsonTest: function() {
+
+            var json = 'assets/js/example-product-list-data-json2.js';
+
+            _$.getJSON( json, function( data ) {
+
+                var categories = data[0].ProductList.Categories;
+
+
+                console.log(categories);
+
+
+
+
+            });
+
+
+
+
+
+
+            // _$.getJSON( "assets/js/example-product-list-data-json2.js", function( data ) {
+
+            //    console.log(data[0]);
+
+
+
+
+            // });
+
+                // var arr = _$.map(categories, function(el) { return el; })
+
+                // console.log(arr);
+
+                // _$.each( data, function( key, val ) {
+                //     items.push( "<li id='" + key + "'>" + val + "</li>" );
+                // });
+                // _$( "<ul/>", {
+                //     "class": "my-new-list",
+                //     html: items.join( "" )
+                // }).appendTo( "body" );
+
         }
+
     };
 
     if(_$('[data-template=user-landing]').length > 0){
-        app.ul.init();   
-    }    
+        app.ul.init();
+    }
 });
